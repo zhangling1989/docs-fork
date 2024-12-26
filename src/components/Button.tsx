35f6206aa16c90d9bdaf1619d6229403f3ceb045
ink-victor <171172553+ink-victor@users.noreply.github.com>
2024-12-26 06:18:49 -0500

    feat: ink-701 add ink network buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,23 +4,26 @@ import clsx from "clsx";
 interface ButtonProps {
   variant: "primary" | "secondary";
   onClick?: () => void;
+  className?: string;
 }
 
 export const Button: React.FC<PropsWithChildren<ButtonProps>> = ({
   children,
   variant,
   onClick,
+  className,
 }) => {
   return (
     <button
       className={clsx(
-        "font-bold py-5 px-8 inline-flex items-center transition-all rounded-full text-xl backdrop-blur-[32px]",
+        "font-bold py-5 px-8 inline-flex items-center justify-center transition-all rounded-full text-xl backdrop-blur-[32px]",
         {
-          "text-magic-white bg-magic-purple shadow-[0px_3px_84px_-10px_rgba(63,107,175,0.5)]":
+          "text-magic-white bg-magic-purple hover:opacity-90 shadow-[0px_3px_84px_-10px_rgba(63,107,175,0.5)]":
             variant === "primary",
           "text-magic-purple bg-magic-semi-deep-purple/15 shadow-[0px_3px_84px_-10px_rgba(63,107,175,0.5)]":
             variant === "secondary",
-        }
+        },
+        className
       )}
       onClick={onClick}
     >
